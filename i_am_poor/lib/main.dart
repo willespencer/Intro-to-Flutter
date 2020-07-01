028bcb81674aeab46ce1df2bbc070af0ee81b17e
Will Spencer <will.e.spencer@gmail.com>
2020-07-01 17:11:22 -0400

    Scaffold app

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -8,6 +8,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white12,
+          title: Text('I Am Poor'),
+        ),
+        backgroundColor: Colors.black26,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/coal.png'),
+          ),
+        ),
+      ),
+    );
   }
 }
