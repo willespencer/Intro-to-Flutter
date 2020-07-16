1b3063aed1684cb06202e8c06c26c61e550e4ff6
Will Spencer <will.e.spencer@gmail.com>
2020-07-16 16:28:27 -0400

    Added profile picture and name

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage('assets/profile.jpg'),
+          ),
+          Text('Will Spencer',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+        ])),
       ),
-    ),
-  );
+    );
+  }
 }
