a60cd9748a83b65f11eb25f61ece6b602c9f51af
Will Spencer <will.e.spencer@gmail.com>
2020-07-16 17:47:22 -0400

    Simplify and prettify with cards and listtiles

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,77 +11,75 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(children: <Widget>[
-          CircleAvatar(
-            radius: 50.0,
-            backgroundImage: AssetImage('assets/profile.jpg'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/profile.jpg'),
+              ),
+              Text(
+                'Will Spencer',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'LEARNING PATH INTERN',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '(703) 483-5414',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'will.spencer@mongodb.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Text(
-            'Will Spencer',
-            style: TextStyle(
-              fontFamily: 'Pacifico',
-              fontSize: 40.0,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'LEARNING PATH INTERN',
-            style: TextStyle(
-              fontFamily: 'SourceSansPro',
-              fontSize: 20.0,
-              color: Colors.teal.shade100,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.5,
-            ),
-          ),
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: Row(
-              children: <Widget>[
-                Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  '(703) 483-5414',
-                  style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'will.spencer@mongodb.com',
-                  style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0),
-                ),
-              ],
-            ),
-          )
-        ])),
+        ),
       ),
     );
   }
