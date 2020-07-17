5a1c697c08663c8899e7eaddf78d4989989761a4
Will Spencer <will.e.spencer@gmail.com>
2020-07-17 17:22:46 -0400

    Challenge 2: update both dice when any clicked

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -27,29 +27,29 @@ class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            rightDiceNumber = Random().nextInt(6) + 1;
+            leftDiceNumber = Random().nextInt(6) + 1;
+          });
+        },
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
             ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
-              child: Image.asset('images/dice$rightDiceNumber.png'),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
