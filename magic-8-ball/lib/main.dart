3ae9ee10786404cd39b59de90bfe28f0ccb39c27
Will Spencer <will.e.spencer@gmail.com>
2020-07-19 16:00:39 -0400

    Create stateless and stateful widgets

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -2,6 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask me Anything'),
+      ),
+      body: Ball(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
