3d5dceb51f0f0ebd8d2751200c870c02e1b9a5d3
Will Spencer <will.e.spencer@gmail.com>
2020-07-20 14:21:54 -0400

    Refactor with new Question class

--- quizzler-flutter-master/lib/main.dart
+++ quizzler-flutter-master/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,16 +27,19 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
-  List<bool> answers = [false, true, true];
+
   int currIndex = 0;
 
   void handleButtonClick(bool answer) {
-    if (answer == answers[currIndex]) {
+    if (answer == questionBank[currIndex].questionAnswer) {
       scoreKeeper.add(
         Icon(Icons.check, color: Colors.green),
       );
@@ -59,7 +63,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[currIndex],
+                questionBank[currIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
