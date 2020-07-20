bed4eae9bf8d9e4b9d7af916f402e6a3c4098c68
Will Spencer <will.e.spencer@gmail.com>
2020-07-20 16:58:57 -0400

    Add abstraction by making a quiz brain

--- quizzler-flutter-master/lib/main.dart
+++ quizzler-flutter-master/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,19 +27,12 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
+  QuizBrain quizBrain = QuizBrain();
 
   int currIndex = 0;
 
   void handleButtonClick(bool answer) {
-    if (answer == questionBank[currIndex].questionAnswer) {
+    if (answer == quizBrain.questionBank[currIndex].questionAnswer) {
       scoreKeeper.add(
         Icon(Icons.check, color: Colors.green),
       );
@@ -63,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[currIndex].questionText,
+                quizBrain.questionBank[currIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
