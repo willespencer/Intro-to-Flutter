e0887f1513225768e2be8d07dc151180a194a01a
Will Spencer <will.e.spencer@gmail.com>
2020-07-21 09:43:13 -0400

    Create StoryBrain class

--- destini/lib/story.dart
+++ destini/lib/story.dart
@@ -3,9 +3,9 @@ class Story {
   String choice1;
   String choice2;
 
-  Story(String title, String c1, String c2) {
-    storyTitle = title;
-    choice1 = c1;
-    choice2 = c2;
+  Story({String storyTitle, String choice1, String choice2}) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
   }
 }
